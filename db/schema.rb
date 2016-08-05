489535a646e0d2ddfad52868e7dcce62d0b5e297
pinzonjulian <julianpinzoneslava@gmail.com>
2016-08-05 00:45:49 -0500

    Remove start time column. Will be using created_at instead

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805035752) do
+ActiveRecord::Schema.define(version: 20160805054501) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "message"
