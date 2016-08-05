de127424f5ac02ad5caf36731b3c913a150d3ec3
pinzonjulian <julianpinzoneslava@gmail.com>
2016-08-05 06:26:31 -0500

    Add validation for date. Change input for datetime

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
+  def end_time_before_now?
+    if end_time < DateTime.now
+      errors.add :end_date, "must be after now"
+    end
+  end
+
   validates :message, presence: true, length: { maximum: 140 }
   validates :end_time, presence: true
+
+  validate :end_time_before_now?
+
+
 
 end
